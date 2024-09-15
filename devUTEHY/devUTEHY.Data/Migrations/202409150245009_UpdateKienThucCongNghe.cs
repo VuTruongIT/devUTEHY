e4368059bed46f24ec37ab06d83c41230fe86d13
truong <vuvantruongit2003@gmail.com>
2024-09-15 23:53:08 +0700

    Update database test KienThucCongNghe

---
+++ devUTEHY/devUTEHY.Data/Migrations/202409150245009_UpdateKienThucCongNghe.cs
@@ -0,0 +1,18 @@
+﻿namespace TeduShop.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UpdateKienThucCongNghe : DbMigration
+    {
+        public override void Up()
+        {
+            DropColumn("dbo.KienThucCongNghe", "STT");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.KienThucCongNghe", "STT", c => c.Int());
+        }
+    }
+}
---
+++ devUTEHY/devUTEHY.Data/Migrations/202409150648577_UpdateKienThucCongNgheL2.cs
@@ -0,0 +1,18 @@
+﻿namespace TeduShop.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UpdateKienThucCongNgheL2 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.KienThucCongNghe", "STT", c => c.Int());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.KienThucCongNghe", "STT");
+        }
+    }
+}
